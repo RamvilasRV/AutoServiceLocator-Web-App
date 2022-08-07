6dc08e6038df86e25dae663f6fceedaa1b811952
Ramvilas <72241424+RamvilasRV@users.noreply.github.com>
2022-08-07 23:36:21 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-83(#ivws0kh_1i=bb5#bh9oaya2=2h@*o7m1s$p_4n-m@_&chq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','find-service-centers.herokuapp.com']
 
